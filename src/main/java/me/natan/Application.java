8c63df031779686c6a9853e9b8c78bed6841f05f
natanstark <132853635+natanstark@users.noreply.github.com>
2023-10-22 22:46:17 -0300

    bugfix cors

--- src/main/java/me/natan/Application.java
+++ src/main/java/me/natan/Application.java
@@ -1,8 +1,10 @@
 package me.natan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
