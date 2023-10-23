b169f76009154da287dd670c41122566c0925759
natanstark <132853635+natanstark@users.noreply.github.com>
2023-10-22 21:59:14 -0300

    implementação de tratamento de exceções

---
+++ src/main/java/me/natan/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package me.natan.controller.exception;
+
+import java.util.NoSuchElementException;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	 private final Logger logger  = (Logger) LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException){
+		return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNotFounfException(NoSuchElementException notFounfException){
+		return new ResponseEntity<>("Resource Id not found", HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(Throwable.class)
+	public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
+		var message = "Unexpected server error, see the logs.";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
+	
